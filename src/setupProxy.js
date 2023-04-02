381971e0c3a146998b6c143ece64b1b7831eb540
Kgdev <48225800+kennethoo@users.noreply.github.com>
2023-04-01 23:57:40 -0700

    new commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,13 @@
 const createProxyMiddleware = require("http-proxy-middleware");
+const proxyUrl =
+  process.env.NODE_ENV == "production"
+    ? "https://myfitsta-backend-app-server.herokuapp.com/"
+    : "http://localhost:8000/";
 module.exports = (app) => {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8000/",
+      target: proxyUrl,
       changeOrigin: true,
     })
   );
