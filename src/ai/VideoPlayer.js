0af01a8ee4288a02bed9ba3fa9429ab2a1179e73
Kgdev <48225800+kennethoo@users.noreply.github.com>
2023-05-14 23:26:35 -0700

    new commit

--- src/ai/VideoPlayer.js
+++ src/ai/VideoPlayer.js
@@ -1,81 +1,84 @@
 import React, { useEffect, useRef, useState } from "react";
 let options = {
-    inputs: 34,
-    outputs: 3,
-    task: 'classification',
-    debug: true
-  }
+  inputs: 34,
+  outputs: 3,
+  task: "classification",
+  debug: true,
+};
 
 function VideoPlayer() {
-    const videoPlayer = useRef();
+  const videoPlayer = useRef();
   const [stateplaying, setStateplaying] = useState(false);
   const [videoStart, setVideoStart] = useState(false);
-  const [poseNet,setPoseNet]=useState(null);
-  const [brain,setBrain]=useState(null);
+  const [poseNet, setPoseNet] = useState(null);
+  const [brain, setBrain] = useState(null);
 
-  function LoadAi(){
-    stateplaying=true
+  function LoadAi() {
+    stateplaying = true;
     // eslint-disable-next-line no-undef
     poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
-  let options = {
-    inputs: 34,
-    outputs: 3,
-    task: 'classification',
-    debug: true
+    poseNet.on("pose", gotPoses);
+    let options = {
+      inputs: 34,
+      outputs: 3,
+      task: "classification",
+      debug: true,
+    };
+    // eslint-disable-next-line no-undef
+    setBrain(ml5.neuralNetwork(options));
   }
-  // eslint-disable-next-line no-undef
-  setBrain(ml5.neuralNetwork(options))
 
-}
-  
   async function start() {
-    if (!stateplaying &&videoPlayer) {
-        // const stream =await  navigator.mediaDevices.getUserMedia({video:true,audio:false})
-        //   videoPlayer.current.srcObject=stream
-        //   videoPlayer.current.play()
-           // eslint-disable-next-line no-undef
-           const pp= ml5.imageClassifier('MobileNet', modelLoaded);
-           setPoseNet(pp)
-           var image = new Image();
-           image.crossOrigin = "anonymous"
-           image.src = "https://myfitsta.s3.us-east-2.amazonaws.com/29f4f55f-de9e-469d-bb95-44f57e9339f0.jpeg";
-           image.width=500
-           image.height=500
-           pp.classify(image, gotPoses);
-          
-           console.log(videoPlayer.current)
-           setVideoStart(true)
+    if (!stateplaying && videoPlayer) {
+      // const stream =await  navigator.mediaDevices.getUserMedia({video:true,audio:false})
+      //   videoPlayer.current.srcObject=stream
+      //   videoPlayer.current.play()
+      // eslint-disable-next-line no-undef
+      const pp = ml5.imageClassifier("MobileNet", modelLoaded);
+      setPoseNet(pp);
+      var image = new Image();
+      image.crossOrigin = "anonymous";
+      image.src =
+        "https://myfitsta.s3.us-east-2.amazonaws.com/29f4f55f-de9e-469d-bb95-44f57e9339f0.jpeg";
+      image.width = 500;
+      image.height = 500;
+      pp.classify(image, gotPoses);
+
+      console.log(videoPlayer.current);
+      setVideoStart(true);
     } else {
       console.log("already playing");
     }
   }
 
   function modelLoaded() {
-    console.log('poseNet ready');
+    console.log("poseNet ready");
   }
   function gotPoses(poses) {
-    console.log(poses)
-    if(!poses) return
-    if (poses.length > 0 ) { 
+    console.log(poses);
+    if (!poses) return;
+    if (poses.length > 0) {
       let pose = poses[0].pose;
       let skeleton = poses[0].skeleton;
     }
   }
-  if(poseNet){
+  if (poseNet) {
     // poseNet.on('pose', gotPoses)
-}
+  }
 
   useEffect(() => {
-setTimeout(()=>{
-    start();
-},3000)
+    setTimeout(() => {
+      start();
+    }, 3000);
   }, []);
 
   return (
     <div className="wraperr-box-video">
-        {/* <video className="vivv" ></video> */}
-        <img   ref={videoPlayer} src="https://myfitsta.s3.us-east-2.amazonaws.com/29f4f55f-de9e-469d-bb95-44f57e9339f0.jpeg"  />
+      {/* <video className="vivv" ></video> */}
+      <img
+        ref={videoPlayer}
+        src="https://myfitsta.s3.us-east-2.amazonaws.com/29f4f55f-de9e-469d-bb95-44f57e9339f0.jpeg"
+      />
     </div>
   );
 }
